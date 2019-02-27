72b31a77223ff7883a30c588d80f07ec59a27650
Rafael Alberto <rafael.alberto1703@gmail.com>
2019-02-27 15:16:28 -0300

    inserindo transactional readonly true para buscas

--- src/main/java/br/com/api/admin/repository/UserRepository.java
+++ src/main/java/br/com/api/admin/repository/UserRepository.java
@@ -2,9 +2,13 @@ package br.com.api.admin.repository;
 
 import br.com.api.admin.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Transactional(readOnly = true)
     Optional<User> findByUsername(String username);
 }
+
