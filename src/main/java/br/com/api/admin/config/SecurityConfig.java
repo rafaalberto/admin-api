29225cf45b6af5fae627f22f4419f7347d1ed455
Rafael Alberto <rafael.alberto1703@gmail.com>
2019-03-02 10:10:27 -0300

    alterando tempo de sessão

--- src/main/java/br/com/api/admin/config/SecurityConfig.java
+++ src/main/java/br/com/api/admin/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/swagger-resources/configuration/security",
             "/swagger-ui.html",
             "/webjars/**",
+            "/auth/**",
     };
 
     @Override
